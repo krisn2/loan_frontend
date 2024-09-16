eb1822e6890d155cef70cc86d667232b47af21e1
krisn2 <krishnasarone08@gmail.com>
2024-09-16 21:32:42 +0530

    motion add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,20 +2,35 @@ import React, { useState } from "react";
 import logo from '../assets/logo.png';
 import openIcon from '../assets/menu/menu.svg';
 import closeIcon from '../assets/menu/close.svg';
+import {motion} from 'framer-motion'
+
+
+const container = (delay) => ({
+  hidden:{ x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay }
+  }
+})
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between py-6 bg-transparent">
+    <nav className="flex items-center justify-between py-6 bg-transparent border-b border-stone-50 ">
       {/* Logo */}
-      <div className="ml-5 mt-5 flex items-center">
+      <motion.div
+      variants={container(0.5)}
+      initial="hidden"
+      animate="visible"
+      className="ml-5 mt-5 flex items-center">
         <img
           src={logo}
           alt="Ofin"
           className="w-40 cursor-pointer transition-transform duration-300 hover:scale-110"
         />
-      </div>
+      </motion.div>
 
       {/* Hamburger Icon for Mobile */}
       <div className="mr-5 block lg:hidden">
@@ -38,44 +53,59 @@ const Navbar = () => {
       >
         <ul className="flex flex-col lg:flex-row gap-4 text-3xl">
           <li>
-            <a
-              href="#"
+            <motion.a
+             whileInView={{ opacity: 1, y: 0 }}
+             initial={{ opacity: 0, y: -100 }}
+             transition={{ duration: 1 }}
+              href="Home"
               className="hover:text-pink-400 transition-colors duration-300"
             >
               Home
-            </a>
+            </motion.a>
           </li>
           <li>
-            <a
-              href="#"
+            <motion.a
+             whileInView={{ opacity: 1, y: 0 }}
+             initial={{ opacity: 0, y: -100 }}
+             transition={{ duration: 1.5 }}
+              href="Hero"
               className="hover:text-pink-400 transition-colors duration-300"
             >
               Loans
-            </a>
+            </motion.a>
           </li>
           <li>
-            <a
-              href="#"
+            <motion.a
+             whileInView={{ opacity: 1, y: 0 }}
+             initial={{ opacity: 0, y: -100 }}
+             transition={{ duration: 2 }}
+              href="About"
               className="hover:text-pink-400 transition-colors duration-300"
             >
               About Us
-            </a>
+            </motion.a>
           </li>
           <li>
-            <a
-              href="#"
+            <motion.a
+             whileInView={{ opacity: 1, y: 0 }}
+             initial={{ opacity: 0, y: -100 }}
+             transition={{ duration: 2.5 }}
+              href="Apply"
               className="hover:text-pink-400 transition-colors duration-300"
             >
               Apply
-            </a>
+            </motion.a>
           </li>
           <li>
-            <a
-              href="#"
+            <motion.a
+             whileInView={{ opacity: 1, y: 0 }}
+             initial={{ opacity: 0, y: -100 }}
+             transition={{ duration: 3 }}
+              href="contect"
               className="hover:text-pink-400 transition-colors duration-300"
             >
               Contact Us
-            </a>
+            </motion.a>
           </li>
         </ul>
       </div>
