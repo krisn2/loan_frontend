532684cc08e66e3737620caf594dfa25967404ed
krisn2 <krishnasarone08@gmail.com>
2024-09-16 20:05:17 +0530

    description

--- src/constant/index.js
+++ src/constant/index.js
@@ -9,52 +9,52 @@ import logoEducation from "../assets/loans/study_loan.jpg";
 
 export const LOANS = [
     {
-        title: "Business Loan",
+        title: "BUSINESS LOAN",
         logo: logoBusiness,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for a business loan in India can range from 10% to 44% per year. The interest rate depends on several factors, including. The minimum loan amount has no limit to borrow whereas the maximum loan is up to Rs. 2 crores for unsecured business loans that are collateral free."
     },
     {
-        title: "Equipment Loan",
+        title: "EQUIPMENT LOAN",
         logo: logoEquipment,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for equipment loans can vary depending on the lender, the type of equipment, and the borrower's credit score. The interest rate for equipment financing can range from 7.50% to 24% per annum. The minimum loan amount is ₹10 lakhs and the maximum is ₹50 crores or ₹100 crores, depending on the client's rating."
     },
     {
-        title: "LRD Loan",
+        title: "LEASE RENTAL DISCOUNTING LOAN (LRD)",
         logo: logoLRD,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for LRD is between 8% and 11.4% depending on the amount of the rental income."
     },
     {
-        title: "Mortgage Loan",
+        title: "LAP LOAN / MORTGAGE LOAN",
         logo: logoMortgage,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for a LAP can range from 7.25% to 11.65% per annum. The minimum and maximum housing loan amount varies depending on the lender, the property's value, and the borrower's eligibility."
     },
     {
-        title: "Personal Loan",
+        title: "PERSONAL LOAN",
         logo: logoPersonal,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "Personal loan interest rates can range from 9.99% to 44% per year, depending on several factors / Depending on your salary, credit score and employment status, you can get a Personal Loan starting from Rs 50,000 up to Rs 50 lakh"
     },
     {
-        title: "Vehicle Loan",
+        title: "CAR LOAN",
         logo: logoVehicle,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for new car loans in India ranged from 6.50% to 14.10%. The interest rate on a car loan is based on the customer's credit score and the Repo Linked Lending Rate (RLLR). ACar Loan is usually give upto 36 times of net monthly salary and 90% of the cost of car value depending on the repaying capacity such that the overall deuctions does not exceed 65% of Gross Income (including proposed EMI)."
     },
     {
-        title: "Education Loan",
+        title: "EDUCATION LOAN",
         logo: logoEducation,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for education loans in India ranges from 8.05% to 15.20% per annum, depending on the bank and the loan scheme. The interest rate may vary depending on the bank's rules and regulations. No collateral needs to be provided for education loans of up to Rs. 40.00 lakh for the identified premier institutes. Maximum Education Loan limit is Rs.125.00 lakh for study in India and Rs.150.00 lakh for study abroad."
     },
     {
-        title: "Machinery Loan",
+        title: "MACHINERY LOAN",
         logo: logoMachinery,
         url: "#",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi officiis, debitis aliquid blanditiis optio"
+        description: "The interest rate for a machinery loan in India can range from 7.50% to 30% per annum. The exact rate depends on a variety of factors, including your CIBIL score, income, and business experience. The minimum loan amount is above ₹10 lakhs and the maximum is ₹50 crores."
     }
 
 ]
